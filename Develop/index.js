2a9b18248ec895f6d3b468f61c4b36531ad88cd4
t-vanderwolk <97120854+t-vanderwolk@users.noreply.github.com>
2022-04-20 17:53:40 -0700

    init() function to call initialized app

--- Develop/index.js
+++ Develop/index.js
@@ -102,4 +102,13 @@ function init() {
 
 
 // Function call to initialize app
-init();
+init()
+.then(userInput => {
+    return generateMarkdown(userInput);
+})
+.then(readmeInfo => {
+    return writeToFile(readmeInfo);
+})
+.catch(err => {
+    console.log(err);
+})
